4e51e5d51e0d02199c4345feadda1b2e224e6c0e
sandybeachsystems <79080861+sandybeachsystems@users.noreply.github.com>
2022-01-09 08:43:49 +0700

    Update Copyright Notice

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,6 +1,28 @@
 /*
   ==============================================================================
 
+    Copyright (c) 2022 - Gordon Webb
+ 
+    This file is part of Wind4Unity2.
+
+    Wind4Unity2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wind4Unity2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wind4Unity2.  If not, see <https://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+/*
+  ==============================================================================
+
     This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
